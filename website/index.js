7d0a793fd7f061357d94f4c7feb5adc5658e45b0
aaronm88 <aaron.d.mcgarvey@gmail.com>
2020-10-06 19:18:14 -0400

    first commit

---
+++ website/index.js
@@ -0,0 +1,29 @@
+/* Function to POST data */
+const postData = async ( url = '/', data = {})=>{
+  console.log(data)
+    const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+
+    try {
+      const newData = await response.json();
+      // console.log(newData);
+      return newData
+    }catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+    }
+}
+
+
+app.listen(3000, function () {
+console.log("Express server listening on port 3000");
+});
+
+// TODO-Call Function
+postData('/addMovie', {movie:'the matrix', score=5})
